dd79c6910b36a0da9d94395b978632efa95a4655
Nishad Mandlik <mandliksg@gmail.com>
2018-07-11 00:24:56 +0530

    Keypad Functionality Improvements

--- Arduino/J-Micro/J-Micro.ino
+++ Arduino/J-Micro/J-Micro.ino
@@ -21,7 +21,8 @@
 #define MOUSE_WH_SNST 0.002
 
 #include<Mouse.h>
-#include<Keyboard.h>
+//#include<JOY-Board.h>
+#include <HID.h>
 
 //Array Of Pins Giving Supply To Key Matrix
 uint8_t KEY_H_OUT[] = {KEY_H0,KEY_H1,KEY_H2,KEY_H3};
@@ -30,10 +31,10 @@ uint8_t KEY_H_OUT[] = {KEY_H0,KEY_H1,KEY_H2,KEY_H3};
 uint8_t KEY_V_IN[] = {KEY_V0,KEY_V1,KEY_V2,KEY_V3};
 
 //Array Of Key Lower Functionalities
-char KEY_MAP0[][4] = {{'1', '2', '3', 'a'}, {'4', '5', '6', 'b'}, {'7', '8', '9', 'c'}, {'-', '0', '=', 'd'}};
+uint16_t KEY_MAP0[][4] = {{KEY_1, KEY_2, KEY_3, KEY_CUT}, {KEY_4, KEY_5, KEY_6, KEY_COPY}, {KEY_7, KEY_8, KEY_9, KEY_PASTE}, {KEY_UNDO, KEY_0, KEY_AGAIN, 0}};
 
 //Array Of Key Upper Functionalities
-char KEY_MAP1[][4] = {{'!', '@', '#', 'A'}, {'$', '%', '^', 'B'}, {'&', '*', '(', 'C'}, {'_', ')', '+', 'D'}};
+uint16_t KEY_MAP1[][4] = {{KEY_BACKSPACE, KEY_UP, KEY_DELETE, 0}, {KEY_LEFT, KEY_ENTER, KEY_RIGHT, 0}, {0, KEY_DOWN, 0, 0}, {KEY_VOLUME_DOWN, KEY_VOLUME_MUTE, KEY_VOLUME_UP, 0}};
 
 bool KEY_PRESSED_NOW = false;
 bool KEY_PRESSED_PREV = false;
@@ -57,6 +58,58 @@ int16_t MOUSE_WH = 0;
 int16_t JOY_X_0 = 512;
 int16_t JOY_Y_0 = 512;
 unsigned long JOY_RESPOND = 0;
+
+uint16_t consRprt;
+
+struct {
+  uint8_t modifierStat;
+  uint8_t reservedByte;
+  uint8_t keyCode;  
+}keybRprt;
+
+static const uint8_t HID_reportDescriptor[] PROGMEM = {
+  /* Consumer Control */
+  0x05, 0x0C,         // USAGE_PAGE (Consumer Device)
+  0x09, 0x01,         // USAGE (Consumer Control)
+  0xA1, 0x01,         // COLLECTION (Application)
+  0x85, 0x04,         //    REPORT_ID (4)
+  0x15, 0x00,         //    LOGICAL_MINIMUM (3FF)
+  0x26, 0xFF, 0x03,   //    LOGICAL_MAXIMUM (3FF)
+  0x19, 0x00,         //    USAGE_MINIMUM (0)
+  0x2A, 0xFF, 0x03,   //    USAGE_MAXIMUM (0x3FF)
+  0x95, 0x01,         //    REPORT_COUNT (1)
+  0x75, 0x10,         //    REPORT_SIZE (16)
+  0x81, 0x00,         //    INPUT (Data,Ary,Abs)
+  0xC0,               //END_COLLECTION
+  /* Keyboard Control */
+  0x05, 0x01,         // USAGE_PAGE (Generic Desktop)  // 47
+  0x09, 0x06,         // USAGE (Keyboard)
+  0xa1, 0x01,         // COLLECTION (Application)
+  0x85, 0x02,         //   REPORT_ID (2)
+  0x05, 0x07,         //   USAGE_PAGE (Keyboard)
+  0x19, 0xe0,         //   USAGE_MINIMUM (Keyboard LeftControl)
+  0x29, 0xe7,         //   USAGE_MAXIMUM (Keyboard Right GUI)
+  0x15, 0x00,         //   LOGICAL_MINIMUM (0)
+  0x25, 0x01,         //   LOGICAL_MAXIMUM (1)
+  0x75, 0x01,         //   REPORT_SIZE (1)
+  0x95, 0x08,         //   REPORT_COUNT (8)
+  0x81, 0x02,         //   INPUT (Data,Var,Abs)
+  0x95, 0x01,         //   REPORT_COUNT (1)
+  0x75, 0x08,         //   REPORT_SIZE (8)
+  0x81, 0x03,         //   INPUT (Cnst,Var,Abs)
+  0x95, 0x06,         //   REPORT_COUNT (1)
+  0x75, 0x08,         //   REPORT_SIZE (8)
+  0x15, 0x00,         //   LOGICAL_MINIMUM (0)
+  0x25, 0x82,         //   LOGICAL_MAXIMUM (128)
+  0x05, 0x07,         //   USAGE_PAGE (Keyboard)
+  0x19, 0x00,         //   USAGE_MINIMUM (Reserved (no event indicated))
+  0x29, 0x82,         //   USAGE_MAXIMUM (Keyboard Locking Caps Lock)
+  0x81, 0x00,         //   INPUT (Data,Ary,Abs)
+  0xc0,               // END_COLLECTION  
+};
+
+static HIDSubDescriptor node(HID_reportDescriptor, sizeof(HID_reportDescriptor));
+HID().AppendDescriptor(&node);
 
 void setup() {
   Serial.begin(9600);
@@ -86,7 +139,11 @@ void setup() {
   JOY_Y_0 = analogRead(JOY_Y);
 
   Mouse.begin();
-  Keyboard.begin();
+  //Keyboard.begin();
+
+  //Initialize By Sending A Blank Report
+  consRprt = 0;
+  HID().SendReport(4,&mediaKey, sizeof(mediaKey));
 }
 
 void loop() { 
@@ -102,16 +159,21 @@ void loop() {
         KEY_PRESSED_NOW = true;
         if (!KEY_PRESSED_PREV) {
           if (BUT_ST_PRESSED) {
+            Serial.println(KEY_MAP1[i][j]);
             Keyboard.press(KEY_MAP1[i][j]);
           }
           else {
-            Keyboard.press(KEY_MAP0[i][j]);
+            Serial.println(KEY_MAP0[i][j]);
+            Keyboard.press(KEY_MAP0[i][j]);            
           }
         }
       }
     }
   }
   if (!KEY_PRESSED_NOW && KEY_PRESSED_PREV) {
+    //buf[2] = 0x00;
+    //Serial.write(buf,9);
+    //Consumer.releaseAll();
     Keyboard.releaseAll();
   }
   KEY_PRESSED_PREV = KEY_PRESSED_NOW;
